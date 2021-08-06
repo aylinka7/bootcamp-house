68a49a63d470cad7d7a387726eaf5986b165bb70
Ailin <ayladylova@gmail.com>
2021-08-06 15:03:04 +0600

    starting heading by Ailin

---
+++ src/views/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import css from "./header.module.css";
+
+export function Header() {
+    return (
+        <div className={css.header}>
+            Header
+        </div>
+    );
+}
+
