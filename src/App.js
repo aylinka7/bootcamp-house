63a962fb4919d0481e69f0f6139c7a7c7fd19743
Kanymgul <jamalova8891@gmail.com>
2021-08-10 16:26:28 +0600

    icons&flats

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { Flats } from "./view/components/flats/Flats";
+import { Icons } from "./view/components/icons/Icons";
 
 function App() {
   return (
     <div>
-
+        <Icons/>
+        <Flats/>
     </div>
   );
 }
