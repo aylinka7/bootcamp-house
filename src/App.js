bf8bc1fdec100dcd3102d691eb707afa3c7e646a
Ailin <ayladylova@gmail.com>
2021-08-12 14:15:48 +0600

    view

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import {Header} from "./views/components/header/Header";
+import {Header} from "./view/components/header/Header";
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
 } from "react-router-dom";
-import Auth from "./views/pages/auth/Auth";
-import Dashboard from "./views/pages/dashboard/Dashboard";
-import Detail from "./views/pages/detail/Detail";
-import Info from "./views/pages/info/Info";
-import Main from "./views/pages/main/Main";
+import Auth from "./view/pages/auth/Auth";
+import Dashboard from "./view/pages/dashboard/Dashboard";
+import Detail from "./view/pages/detail/Detail";
+import Info from "./view/pages/info/Info";
+import Main from "./view/pages/main/Main";
 
 function App() {
   return (
